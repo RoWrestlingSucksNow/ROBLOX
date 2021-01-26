35c5965350012cd1ab94e7a56e4300efd96560e3
RoWrestlingSucksNow <73460929+RoWrestlingSucksNow@users.noreply.github.com>
2021-01-26 14:19:20 -0500

    Update API.lua

--- GymOfTragedy/API.lua
+++ GymOfTragedy/API.lua
@@ -3,6 +3,24 @@ if (getgenv().GOTAPI) then return getgenv().GOTAPI end
 
 
 GOTAPI.BypassAntiCheat = function()
+ local mt = getrawmetatable(game)
+local old = mt.__namecall
+local protect = newcclosure or protect_function
+
+if not protect then
+protect = function(f) return f end
+end
+
+setreadonly(mt, false)
+mt.__namecall = protect(function(self, ...)
+local method = getnamecallmethod()
+if method == "Kick" then
+wait(9e9)
+return
+end
+return old(self, ...)
+end)
+hookfunction(game:GetService("Players").LocalPlayer.Kick,protect(function() wait(9e9) end))
 for i,v in pairs(game.ReplicatedStorage:GetChildren()) do
 if v.Name == "EDEvents" or v.Name == "BigCleanEvents" then
 v:Destroy()
